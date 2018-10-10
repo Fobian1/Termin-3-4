8da1e5f2c15587dbd551387dd9a1818efce48e5d
Fabian Ramel <37147537+Fobian1@users.noreply.github.com>
2018-10-10 14:16:43 +0200

    Quicksort

--- Algoritm/Merge/AlgoritmExpriment/Mainprogram.cs
+++ Algoritm/Merge/AlgoritmExpriment/Mainprogram.cs
@@ -130,5 +130,40 @@ public static class MyInsertionTest {
         result.Add(list.First());
         list.RemoveAt(0);
     }
+    //Quicksort ->
+    public static void Quicksort(IComparable[] elements, int left, int right) {
+        int i = left, j = right;
+        IComparable partitioning = elements[(left + right) / 2];
+
+        while (i <= j) {
+            while (elements[i].CompareTo(partitioning) < 0) {
+                i++;
+            }
+
+            while (elements[j].CompareTo(partitioning) > 0) {
+                j--;
+            }
+
+            if (i <= j) {
+                // Swap
+                IComparable tmp = elements[i];
+                elements[i] = elements[j];
+                elements[j] = tmp;
+
+                i++;
+                j--;
+            }
+        }
+
+        // Recursive calls
+        if (left < j) {
+            Quicksort(elements, left, j);
+        }
+
+        if (i < right) {
+            Quicksort(elements, i, right);
+        }
+    }
+
 }
 
