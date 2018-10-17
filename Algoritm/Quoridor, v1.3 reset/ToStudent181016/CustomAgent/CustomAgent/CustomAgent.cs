904e04c339e157a0e052ed1002f684eaf8bbfa54
Fabian Ramel <37147537+Fobian1@users.noreply.github.com>
2018-10-17 17:43:48 +0200

    Quoridor

---
+++ Algoritm/Quoridor, v1.3 reset/ToStudent181016/CustomAgent/CustomAgent/CustomAgent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quoridor.AI
+{
+    class CustomAgent : Agent
+    {
+        public static void Main()
+        {
+            new CustomAgent().Start();
+        }
+
+        public override Action DoAction(GameData status)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
